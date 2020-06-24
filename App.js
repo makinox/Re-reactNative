6e09efdaab2c060c593bba955f0a06c587e5abda
Jesús Bossa <x@jesusbossa.dev>
2020-06-24 11:16:54 -0500

    [IMPROVEMENT] Navbar color

--- App.js
+++ App.js
@@ -1,28 +1,38 @@
-// import React from 'react';
-// import Main from './src/pages/main/main';
-// import Profile from './src/pages/profile/profile';
-import BottomTab from './src/components/bottom-tab/bottom-tab';
-
-// const App = () => <Main />;
-
-// export default App;
-
 import * as React from 'react';
+import {StyleSheet} from 'react-native';
+import {createMaterialBottomTabNavigator} from '@react-navigation/material-bottom-tabs';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import {NavigationContainer} from '@react-navigation/native';
-// import {createStackNavigator} from '@react-navigation/stack';
+import HomeScreen from './src/pages/home/home';
+import profileScreen from './src/pages/settings/settings';
 
-// const Stack = createStackNavigator();
+const Tab = createMaterialBottomTabNavigator();
 
-export default function MyStack() {
-  return (
-    // <NavigationContainer>
-    //   <Stack.Navigator>
-    //     <Stack.Screen name="Home" component={Main} options={{title: 'Welcome'}} />
-    //     <Stack.Screen name="Profile" component={Profile} />
-    //   </Stack.Navigator>
-    // </NavigationContainer>
-    <NavigationContainer>
-      <BottomTab />
-    </NavigationContainer>
-  );
-}
+export default () => (
+  <NavigationContainer>
+    <Tab.Navigator initialRouteName="Home" activeColor="#ffffff" inactiveColor="#3e2465" barStyle={styles.TabNavigator}>
+      <Tab.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{
+          tabBarLabel: 'Home',
+          tabBarIcon: ({color, size}) => <MaterialCommunityIcons name="home" color={color} size={size} />,
+        }}
+      />
+      <Tab.Screen
+        name="Settings"
+        component={profileScreen}
+        options={{
+          tabBarLabel: 'Settings',
+          tabBarIcon: ({color, size}) => <MaterialCommunityIcons name="bell" color={color} size={size} />,
+        }}
+      />
+    </Tab.Navigator>
+  </NavigationContainer>
+);
+
+const styles = StyleSheet.create({
+  TabNavigator: {
+    backgroundColor: '#61DAFB',
+  },
+});
