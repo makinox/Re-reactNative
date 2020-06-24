5768a5f2bd329fe844b9a0ee967f66165317329e
Jesús Bossa <x@jesusbossa.dev>
2020-06-24 12:24:36 -0500

    [ADD] Library tested

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,22 +1,69 @@
-import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
-import {Button, withTheme, Appbar} from 'react-native-paper';
+/* eslint-disable react-hooks/rules-of-hooks */
+import React, {useState} from 'react';
+import {View, Text, StyleSheet, Image} from 'react-native';
+import {Button, withTheme, Appbar, Avatar, Badge, ActivityIndicator, Banner} from 'react-native-paper';
 
-const Home = ({theme}) => (
-  <>
-    <Appbar.Header>
-      <Appbar.Content title="Home" subtitle={'Subtitle'} />
-    </Appbar.Header>
+const Home = ({theme}) => {
+  const [visible, useVisible] = useState(true);
+  const dismissBanner = () => useVisible(false);
+  return (
+    <>
+      <Appbar.Header>
+        <Appbar.Content title="Home" subtitle={'Subtitle'} />
+        <Appbar.Action icon="magnify" onPress={() => {}} />
+        <Appbar.Action icon="dots-vertical" onPress={() => {}} />
+      </Appbar.Header>
+      <Banner
+        visible={visible}
+        actions={[
+          {
+            label: 'Fix it',
+            onPress: () => dismissBanner(),
+          },
+          {
+            label: 'Learn more',
+            onPress: () => dismissBanner(),
+          },
+        ]}
+        icon={({size}) => (
+          <Image
+            source={{uri: 'https://avatars3.githubusercontent.com/u/17571969?s=400&v=4'}}
+            style={{
+              width: size,
+              height: size,
+            }}
+          />
+        )}>
+        There was a problem processing a transaction on your credit card.
+      </Banner>
 
-    <View style={styles.container}>
-      <Text>Home!</Text>
-      <Text style={{color: theme.colors.primary}}>Yo!</Text>
-      <Button theme={{roundness: 3}} icon="camera">
-        Press me
-      </Button>
-    </View>
-  </>
-);
+      <View style={styles.container}>
+        <Text>Home!</Text>
+        <Text style={{color: theme.colors.primary}}>Yo!</Text>
+        <Button theme={{roundness: 3}} icon="camera">
+          Press me
+        </Button>
+        <View>
+          <Avatar.Icon size={24} icon="folder" />
+        </View>
+        <View>
+          <Avatar.Text size={24} label="XD" />
+        </View>
+        <View>
+          <Badge>3</Badge>
+        </View>
+        <View>
+          <ActivityIndicator animating={true} color={'#61DAFB'} size={80} />
+        </View>
+        <View>
+          <Button icon="camera" mode="contained" onPress={() => console.log('Pressed')}>
+            Press me
+          </Button>
+        </View>
+      </View>
+    </>
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
--- src/pages/settings/settings.js
+++ src/pages/settings/settings.js
@@ -1,22 +1,60 @@
-import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
-import {Appbar} from 'react-native-paper';
+/* eslint-disable react-hooks/rules-of-hooks */
+import React, {useState} from 'react';
+import {View, ScrollView} from 'react-native';
+import {Appbar, Avatar, Button, Card, Title, Paragraph, Dialog, Portal} from 'react-native-paper';
 
-export default () => (
-  <>
-    <Appbar.Header>
-      <Appbar.Content title="Settings" subtitle={'Subtitle'} />
-    </Appbar.Header>
-    <View style={styles.container}>
-      <Text>Settings!</Text>
-    </View>
-  </>
-);
+const LeftContent = props => <Avatar.Icon {...props} icon="folder" />;
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-});
+export default () => {
+  const [visible, useVisible] = useState(false);
+  const dismissBanner = () => useVisible(false);
+  const showBanner = () => useVisible(true);
+  return (
+    <>
+      <Appbar.Header>
+        <Appbar.BackAction onPress={() => {}} />
+        <Appbar.Content title="Settings" subtitle={'Subtitle'} />
+      </Appbar.Header>
+      <ScrollView>
+        <View>
+          <Card>
+            <Card.Title title="Card Title" subtitle="Card Subtitle" left={LeftContent} />
+            <Card.Content>
+              <Title>Esto es un titulo</Title>
+              <Paragraph>Card content</Paragraph>
+            </Card.Content>
+            <Card.Cover source={{uri: 'https://picsum.photos/700'}} />
+            <Card.Actions>
+              <Button>Cancel</Button>
+              <Button>Ok</Button>
+            </Card.Actions>
+          </Card>
+        </View>
+        <View>
+          <Button mode="contained" onPress={showBanner}>
+            Show Dialog
+          </Button>
+          <Portal>
+            <Dialog visible={visible} onDismiss={dismissBanner}>
+              <Dialog.Title>Alert</Dialog.Title>
+              <Dialog.Content>
+                <Paragraph>This is simple dialog</Paragraph>
+              </Dialog.Content>
+              <Dialog.Actions>
+                <Button onPress={dismissBanner}>Done</Button>
+              </Dialog.Actions>
+            </Dialog>
+          </Portal>
+        </View>
+      </ScrollView>
+    </>
+  );
+};
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     justifyContent: 'center',
+//     alignItems: 'center',
+//   },
+// });
