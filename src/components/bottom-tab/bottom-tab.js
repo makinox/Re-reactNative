283791808719a2a66d51b6b7961b8028be047052
Jesús Bossa <x@jesusbossa.dev>
2020-06-23 21:29:47 -0500

    [ADD] Tab icons

--- src/components/bottom-tab/bottom-tab.js
+++ src/components/bottom-tab/bottom-tab.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import HomeScreen from '../../pages/main/main';
 import profileScreen from '../../pages/profile/profile';
 
@@ -8,8 +9,22 @@ const Tab = createBottomTabNavigator();
 export default function MyTabs() {
   return (
     <Tab.Navigator initialRouteName="Home">
-      <Tab.Screen name="Home" component={HomeScreen} />
-      <Tab.Screen name="Settings" component={profileScreen} />
+      <Tab.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{
+          tabBarLabel: 'Home',
+          tabBarIcon: ({color, size}) => <MaterialCommunityIcons name="home" color={color} size={size} />,
+        }}
+      />
+      <Tab.Screen
+        name="Settings"
+        component={profileScreen}
+        options={{
+          tabBarLabel: 'Settings',
+          tabBarIcon: ({color, size}) => <MaterialCommunityIcons name="bell" color={color} size={size} />,
+        }}
+      />
     </Tab.Navigator>
   );
 }
